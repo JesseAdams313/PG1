d179f5e489f37d5813d1a7fde5042d8a9273eea5
Jesse Adams <JSAdams@student.fullsail.edu>
2023-09-27 20:46:09 -0700

    UI Setup

---
+++ Hello World!/PokemonExample/PokemonExample/UI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonExample
+{
+    public static class UI
+    {
+        /// <summary>
+        /// Clears console and prints a given text argument as a formatted dialog with key continue.
+        /// </summary>
+        /// <param name="text"></param>
+
+        public static void ShowDialog(string text)
+        {
+            Console.Clear();
+            Console.WriteLine(text);
+
+            Console.WriteLine("\n\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static string PromptUserName(string userName)
+        {
+            Console.Clear();
+            Console.Write("Name: ");
+            return Console.ReadLine();
+        }
+    }
+}
