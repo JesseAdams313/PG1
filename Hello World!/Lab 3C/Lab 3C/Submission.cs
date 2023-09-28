29d649032debb73ab1acab1a9dafe5d0c46d8949
Jesse Adams <JSAdams@student.fullsail.edu>
2023-09-27 19:56:43 -0700

    Trying to Fix merge and pushes

--- Hello World!/Lab 3C/Lab 3C/Submission.cs
+++ Hello World!/Lab 3C/Lab 3C/Submission.cs
@@ -25,6 +25,7 @@ namespace FSPG1
         // Test 1 - write an overloaded constructor that will accept 
         // a string value (only a string) to intialize retort.  Do 
         // not accept an int and do not initialize hidden
+//<<<<<<< HEAD
         public Submission(string retort)
         {
             this.retort = retort;
@@ -54,6 +55,23 @@ namespace FSPG1
             get { return hidden; }
             set { hidden = value; }
         }
+//=======
+
+
+        // Test 2 - Write a getter for retort (GetRetort)
+        // Getters return the data type matching the field they are
+        // 'getting' and do not accept parameters (for now)
+
+
+        // Test 3 - Write a setter for retort (SetRetort)
+        // Setters don't return anything and accept a parameter that
+        // matches the member field they are 'setting'
+
+
+        // Test 4 - Write a property for hidden named Hidden
+        // The property must update/access the hidden member field
+
+//>>>>>>> Setup
 
         // Test 5 - Write a method named YellAtMe that accepts
         // no parameters and returns the value of retort as 
@@ -61,16 +79,24 @@ namespace FSPG1
         // a member of the string class (so use the dot operator 
         // to access it from any string. The definition of ToUpper is:
         // public string ToUpper()
+//<<<<<<< HEAD
         public string YellAtMe()
         {
             return retort.ToUpper();
         }
+//=======
+
+//>>>>>>> Setup
 
         // Test 6 - Complete the method so that it returns 
         // the input value multiplied by 4
         public static int Test6(int input)
         {
+//<<<<<<< HEAD
             return input * 4;
+//=======
+            return 0;
+//>>>>>>> Setup
         }
 
         // Test 7 - Complete the method so that it returns
@@ -78,7 +104,11 @@ namespace FSPG1
         // Use Math.Sqrt and cast the returned value to float
         public static float Test7(float input)
         {
+//<<<<<<< HEAD
             return (float)Math.Sqrt(input);
+//=======
+            return 0;
+//>>>>>>> Setup
         }
 
         // Test 8 - Complete the method so that it returns
@@ -90,8 +120,12 @@ namespace FSPG1
 
         public static int Test8(int min, int max, int seed)
         {
+//<<<<<<< HEAD
             Random random = new Random(seed);
             return random.Next(min, max);
+//=======
+            return 0;
+//>>>>>>> Setup
         }
 
         // Test 9 - Complete the method so that it returns 
@@ -100,7 +134,11 @@ namespace FSPG1
 
         public static int Test9(int number1, int number2)
         {
+//<<<<<<< HEAD
             return number1 / number2;
+//=======
+            return 0;
+//>>>>>>> Setup
         }
 
         // Test 10 - Complete the method so that it return
@@ -109,7 +147,11 @@ namespace FSPG1
 
         public static int Test10(int number1, int number2)
         {
+//<<<<<<< HEAD
             return number1 % number2;
+//=======
+            return 0;
+//>>>>>>> Setup
         }
     }
 }
