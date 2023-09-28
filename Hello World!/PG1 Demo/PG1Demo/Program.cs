5f06f4a82b1e19753558cc0f495fea0137bcab06
Jesse Adams <JSAdams@student.fullsail.edu>
2023-09-27 19:21:48 -0700

    SetUp new Repo For PG1

---
+++ Hello World!/PG1 Demo/PG1Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PG1Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Display "Hello!" welcome header
+            Console.WriteLine("========================");
+            Console.WriteLine("\nHello!");
+            Console.WriteLine("========================");
+
+            //Prompt user for their name
+            Console.WriteLine("\nWhat is your name?");
+            Console.Write("Name: ");
+
+            //Grab name from the console and store it in a variable
+            string name = Console.ReadLine();
+
+            //Wecome user with their name...
+            Console.WriteLine("\nWelcome " + name + "!");
+
+            //Prompt user for their age 
+            Console.WriteLine("\nWhat is your age, " + name + "?");
+            Console.Write("Age: ");
+
+            int age = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nWow! {name} is {age}!");
+            Console.WriteLine($"Next Year they will be {age + 1}!!!");
+
+            //Exit application prompt
+            Console.WriteLine("\n\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
